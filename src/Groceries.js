f630e0dcc9545ef786684d920a89037731135051
Rommel Demano <youremail>
2020-12-12 12:19:47 -0500

    first commit

---
+++ src/Groceries.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Groceries = ({ groceries }) => {
+  return (
+    <ul>
+      {groceries.map((curr, idx) => {
+        return <li key={idx}>{curr.name}</li>;
+      })}
+    </ul>
+  );
+};
+
+const mapStateToProps = ({ groceries }) => {
+  return {
+    groceries,
+  };
+};
+
+export default connect(mapStateToProps)(Groceries);
